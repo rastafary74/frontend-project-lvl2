d4d1b6535cf76fe80a3db820448337f6dd0a62b2
Александр Петраков <rastafary74@ya.ru>
2022-02-19 04:40:56 +0300

    Step 2. Add opions for gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,4 +6,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1');
 
+program
+  .option('-f, --format <type>', 'output format');
+
 program.parse();
