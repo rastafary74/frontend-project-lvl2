d41463fe168243561d7585e4f07840ac476ad9e5
Александр Петраков <rastafary74@ya.ru>
2022-02-20 01:11:19 +0300

    Step 2. Setting

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 
-const program = new Command();
-program
+const gendiff = new Command();
+gendiff
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .usage('[options] <filepath1> <filepath2>');
 
-program
+gendiff
   .option('-f, --format <type>', 'output format');
 
-program.parse();
+gendiff.parse();
